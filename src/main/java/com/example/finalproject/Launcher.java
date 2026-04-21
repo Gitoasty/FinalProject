0d6b10bd18927d9659bd58d7aaaf8f4f41e81f85
Gitoasty <65080822+Gitoasty@users.noreply.github.com>
2026-04-21 15:10:25 +0200

    initial commit

---
+++ src/main/java/com/example/finalproject/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.finalproject;
+
+import javafx.application.Application;
+
+public class Launcher {
+    static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
