14eff802e945c686ebeebe5ca05faf84591d69ab
Gitoasty <65080822+Gitoasty@users.noreply.github.com>
2026-04-24 15:20:55 +0200

    dodan volume slider

--- src/main/java/com/example/finalproject/controllers/PlayerController.java
+++ src/main/java/com/example/finalproject/controllers/PlayerController.java
@@ -1,6 +1,5 @@
 package com.example.finalproject.controllers;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -60,19 +59,26 @@ public class PlayerController implements Initializable {
                 progressBar.setValue(newTime.toSeconds());
             }
         });
+        mediaPlayer.setVolume(volumeBar.getValue() * 0.01);
 
         progressBar.setOnMousePressed(e -> mediaPlayer.seek(Duration.seconds(progressBar.getValue())));
         progressBar.setOnMouseDragged(e -> mediaPlayer.seek(Duration.seconds(progressBar.getValue())));
     }
 
     public void playPause() {
-        mediaPlayer.setOnReady(() -> {
-            Duration total = mediaPlayer.getTotalDuration();
-            progressBar.setMax(total.toSeconds());
-            progressBar.setValue(0);
-        });
+        if (mediaPlayer.getStatus().equals(MediaPlayer.Status.PLAYING)) {
+            mediaPlayer.pause();
+        } else if (mediaPlayer.getStatus().equals(MediaPlayer.Status.PAUSED)) {
+            mediaPlayer.play();
+        } else {
+            mediaPlayer.setOnReady(() -> {
+                Duration total = mediaPlayer.getTotalDuration();
+                progressBar.setMax(total.toSeconds());
+                progressBar.setValue(0);
+            });
 
-        mediaPlayer.play();
+            mediaPlayer.play();
+        }
     }
 
     private void stop() {
@@ -84,10 +90,21 @@ public class PlayerController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         updateList();
 
+        volumeBar.setMin(0);
+        volumeBar.setMax(100);
+        volumeBar.setValue(100);
+        volumeBar.setMajorTickUnit(50);
+        volumeBar.setMinorTickCount(10);
+        volumeBar.setShowTickMarks(true);
+        volumeBar.setShowTickLabels(true);
+
+        volumeBar.valueProperty().addListener((observableValue, number, t1) -> mediaPlayer.setVolume(volumeBar.getValue() * 0.01));
+
         progressBar.setMin(0);
 
         songList.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
             if (newVal != null) {
+                songTitle.setText(newVal);
                 System.out.println("Selected: " + newVal);
 
                 if (selected != null) {
